05f72cb0c463757011756df3e86b77c51046ef46
msjung <jms7446@gmail.com>
2019-07-26 21:58:44 +0900

    Monitor, DiffMonitor 추가

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,6 +1,18 @@
 import pytest
 
-from ..text import Matcher, StateMatcher, Match, AhocorasickWrapper
+from ..text import Monitor, DiffMonitor, Match, AhocorasickWrapper, MonitorResult
+
+
+################################################################################
+# Match
+################################################################################
+
+def test_match_make_snippet():
+    match = Match('abc', 3, 6, 'xy abc fgh')
+    assert match.make_snippet(window_size=1) == ' abc '
+    assert match.make_snippet(window_size=2) == 'y abc f'
+    assert match.make_snippet(window_size=3) == 'xy abc fg'
+    assert match.make_snippet(window_size=9) == 'xy abc fgh'
 
 
 ################################################################################
@@ -10,55 +22,62 @@ from ..text import Matcher, StateMatcher, Match, AhocorasickWrapper
 def test_ahocorasick_wrapper_with_allow_substring_match():
     """ahocorasick 기본 동작 확인"""
     keywords = ['abcd', 'bc', 'bcd', 'abc', 'cde']
-    haystack = 'xabcdey'
+    text = 'xabcdey'
     kwtree = AhocorasickWrapper(keywords, allow_substring_match=True)
-    assert list(kwtree.find_all(haystack)) == [
-        ('abc', 1, 4),
-        ('bc', 2, 4),
-        ('abcd', 1, 5),
-        ('bcd', 2, 5),
-        ('cde', 3, 6),
+    assert kwtree.find_all(text) == [
+        Match('abc', 1, 4, text),
+        Match('bc', 2, 4, text),
+        Match('abcd', 1, 5, text),
+        Match('bcd', 2, 5, text),
+        Match('cde', 3, 6, text),
     ]
 
 
 def test_ahocorasick_wrapper_no_substring_match():
     """한 매칭이 다른 매칭에 완전히 포함되면(substring) 무시한다"""
     keywords = ['abcd', 'bc', 'bcd', 'abc', 'cde']
-    haystack = 'xabcdey'
+    text = 'xabcdey'
     kwtree = AhocorasickWrapper(keywords, allow_substring_match=False)
-    assert list(kwtree.find_all(haystack)) == [
-        ('abcd', 1, 5),
-        ('cde', 3, 6),
+    assert list(kwtree.find_all(text)) == [
+        Match('abcd', 1, 5, text),
+        Match('cde', 3, 6, text),
     ]
 
+
+################################################################################
+# Monitor
+################################################################################
 
 def test_matcher_basic_match():
     keywords = ['abc', '사랑']
-    # 앞 뒤로 공백 제외 2글자로 snippet (영문, 한글 동일)
-    matcher = Matcher(keywords=keywords, snippet_window_size=2)
+    # 앞 뒤 3글자로(공백포함) snippet 생성. 영문, 한글 동일한 사이즈로 취급됨
+    monitor = Monitor(keywords=keywords, snippet_window_size=3)
 
-    assert list(matcher.match('xyz 안녕하세요')) == []
-    assert list(matcher.match('가 나 다 abc de f')) == [
-        Match('abc', '나 다 abc de'),
+    assert monitor.check('xyz 안녕하세요').results == []
+    assert monitor.check('가 나 다 abc de f').results == [
+        MonitorResult('abc', ' 다 abc de'),
     ]
-    assert list(matcher.match('가 나 다 abc 라 마 사랑 하늘 end')) == [
-        Match('abc', '나 다 abc 라 마'),
-        Match('사랑', '라 마 사랑 하늘'),
+    assert monitor.check('가 나 다 abc 라마 사랑 하늘 end').results == [
+        MonitorResult('abc', ' 다 abc 라마'),
+        MonitorResult('사랑', '라마 사랑 하늘'),
     ]
 
 
-@pytest.mark.parametrize(['pre_input', 'cur_input', 'expected'], [
-    ('', 'xyz 안녕하세요', []),
-    ('', '가 나 다 abc de f', [('', '나 다 abc de')]),
-    ('가 나 다 abc de f', '가 나 다 abc de f', []),   # 동일한 입력
-    ('가 나 다 abc de f', '가 나 다abcde f', []),   # 공백만 다름
-    ('가 나 다 abc de f', '가 나 도 abc de f', [('다 abc de', '도 abc de')]),   # 키워드 주변이 변함
+@pytest.mark.parametrize(['pre_input', 'cur_input', 'removed', 'added'], [
+    ('', 'xyz 안녕하세요', [], []),
+    ('', '가나다 abc de f', [], [MonitorResult('abc', '나다 abc de')]),   # 생김
+    ('가나다 abc de f', '', [MonitorResult('abc', '나다 abc de')], []),   # 없어짐
+    ('가나다 abc de f', '가나다 abc de f', [], []),   # 동일한 입력
+    # ('가나다 abc de f', '가나다abcde f', [], []),   # 공백만 다름 (구현안됨)
+    ('가나다 abc de f', '가나도 abc de f', [MonitorResult('abc', '나다 abc de')], [MonitorResult('abc', '나도 abc de')]),
 ])
-def test_state_matcher_basic_match1(pre_input, cur_input, expected):
+def test_state_matcher_basic_match1(pre_input, cur_input, removed, added):
     """StateMatcher는 이전 문자열을 참조해서 keyword 주변에 변화가 있지 않으면 무시하는지 확인"""
     keywords = ['abc', '사랑']
     # 앞 뒤로 공백 제외 2글자로 snippet (영문, 한글 동일)
-    matcher = StateMatcher(keywords=keywords, snippet_window_size=1, check_window_size=1)
+    monitor = DiffMonitor(keywords=keywords, snippet_window_size=3)
 
-    matcher.match(pre_input)
-    assert list(matcher.match(cur_input)) == expected
+    monitor.check(pre_input)
+    report = monitor.check(cur_input)
+    assert report.removed_results == removed
+    assert report.added_results == added
--- text.py
+++ text.py
@@ -1,19 +1,52 @@
-from dataclasses import dataclass
+from typing import List
+from dataclasses import dataclass, field
 
 from ahocorasick import Automaton
 
 
+@dataclass
+class Match:
+    keyword: str
+    start: int
+    end: int
+    text: str
+
+    def make_snippet(self, window_size=2):
+        snippet_start = max(0, self.start - window_size)
+        snippet_end = min(len(self.text), self.end + window_size)
+        return self.text[snippet_start:snippet_end]
+
+
+@dataclass
+class MonitorResult:
+    keyword: str
+    snippet: str
+
+
+@dataclass
+class MonitorReport:
+    results: List[MonitorResult] = field(default_factory=list)
+
+
+@dataclass
+class DiffMonitorReport:
+    removed_results: List[MonitorResult] = field(default_factory=list)
+    added_results: List[MonitorResult] = field(default_factory=list)
+    pre_text: str = ''
+
+
 class AhocorasickWrapper:
-    def __init__(self, keywords, allow_substring_match):
+    def __init__(self, keywords: List[str], allow_substring_match=False):
         self.kwtree = Automaton()
         self.allow_substring_match = allow_substring_match
         for keyword in keywords:
             self.kwtree.add_word(keyword, keyword)
         self.kwtree.make_automaton()
 
-    def find_all(self, text):
+    def find_all(self, text: str) -> List[Match]:
         ahocorasick_result = self.kwtree.iter(text)
-        matches = self._alt_result_format(ahocorasick_result)
+        start_end_result = self._alt_result_format(ahocorasick_result)
+        matches = [Match(kw, start, end, text) for kw, start, end in start_end_result]
         if self.allow_substring_match:
             return matches
         else:
@@ -21,41 +54,66 @@ class AhocorasickWrapper:
 
     @staticmethod
     def _alt_result_format(ahocorasick_results):
-        """ note: pyahocorasick 에서 나오는 end 값은 매칭된 마지막 index python slicing 개념으로 start, end를 만든다. """
-        return [(kw, end + 1 - len(kw), end + 1) for end, kw in ahocorasick_results]
+        """pyahocorasick 에서 나오는 end 값은 매칭된 마지막 index, python slicing 개념의 start, end로 바꾼다
+        [(kw, end_index), ...] -> [(kw, start, end), ...]
+        """
+        return [(kw, end_index + 1 - len(kw), end_index + 1) for end_index, kw in ahocorasick_results]
 
     @staticmethod
-    def _exclude_substring_match(matches):
+    def _exclude_substring_match(matches: List[Match]):
         """다름 매칭에 substring으로 포함되는 매칭은 무시한다"""
-        matches = sorted(matches, key=lambda x: (x[1], -x[2]))
+        matches = sorted(matches, key=lambda m: (m.start, -m.end))
         non_substring_matches = []
         max_end = 0
         for match in matches:
-            if match[2] > max_end:
+            if match.end > max_end:
                 non_substring_matches.append(match)
-                max_end = match[2]
+                max_end = match.end
         return non_substring_matches
 
 
-@dataclass(frozen=True)
-class Match:
-    keyword: str
-    snippet: str
-    index: int = 0
-
-
-class Matcher:
+class Monitor:
     """keyword들이 주어진 text에 등장하는지 확인하고 매치된 정보를 제공한다"""
 
     def __init__(self, keywords, snippet_window_size=2):
-        self.keywords = keywords
+        self.kwtree = AhocorasickWrapper(keywords, allow_substring_match=False)
         self.snippet_window_size = snippet_window_size
 
-    def match(self, text):
-        # naive한 구현
-        for keyword in self.keywords:
-            pass
+    def check(self, text: str) -> MonitorReport:
+        matches = self.kwtree.find_all(text)
+        results = [MonitorResult(keyword=match.keyword, snippet=match.make_snippet(self.snippet_window_size))
+                   for match in matches]
+        return MonitorReport(results=results)
 
 
-class StateMatcher:
-    """Matcher와 동일한 기능을 하지만, 이전에 주어진 text를 참조해서 매치된 text 주변에 변화가 있지 않으면 무시한다"""
+class DiffMonitor:
+    """Monitor 비슷하지만, 이전에 주어진 text를 참조해서 매치된 text 주변에 변화가 있지 않으면 무시한다"""
+
+    def __init__(self, keywords, snippet_window_size=2):
+        self.kwtree = AhocorasickWrapper(keywords)
+        self.snippet_window_size = snippet_window_size
+        self.pre_text = ""
+
+    def check(self, text: str) -> DiffMonitorReport:
+        if text == self.pre_text:
+            monitor_report = DiffMonitorReport(pre_text=self.pre_text)
+        else:
+            monitor_report = self._diff_results(text)
+            self.pre_text = text
+        return monitor_report
+
+    def _diff_results(self, new_text):
+        pre_matches = self.kwtree.find_all(self.pre_text)
+        pre_results = [MonitorResult(match.keyword, match.make_snippet(self.snippet_window_size))
+                       for match in pre_matches]
+        pre_snippet_set = {result.snippet for result in pre_results}
+
+        new_matches = self.kwtree.find_all(new_text)
+        new_results = [MonitorResult(match.keyword, match.make_snippet(self.snippet_window_size))
+                       for match in new_matches]
+        new_snippet_set = {result.snippet for result in new_results}
+
+        removed_results = [result for result in pre_results if result.snippet not in new_snippet_set]
+        added_results = [result for result in new_results if result.snippet not in pre_snippet_set]
+
+        return DiffMonitorReport(removed_results, added_results, self.pre_text)
